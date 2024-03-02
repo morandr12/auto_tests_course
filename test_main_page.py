d7f37a72aeb85fddd2398fa14698d47ddc37f756
morand12 <morand12@live.com>
2024-03-02 19:33:17 +0300

    добавлено: модуль login_page.py в locators.py - class LoginPageLocators изменено: в test_main_page.py - test_2_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,21 +1,21 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_1_guest_should_see_login_link(browser):
     """Проверка доступности login_link."""
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.should_be_login_link()
 
 
 def test_2_guest_can_go_to_login_page(browser):
     """Проверка перехода на страницу логина"""
     link = "http://selenium1py.pythonanywhere.com/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page = MainPage(browser=browser, url=link)
+    main_page = MainPage(browser=browser, url=link)
     # открываем страницу и переходим на страницу логина
-    page.open().go_to_login_page()
-
-
-
+    main_page.open().go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
